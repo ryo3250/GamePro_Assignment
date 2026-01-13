a7ebd051f1d3e60927647c253a43a9262d87a155
ryo3250 <ryoman1002@icloud.com>
2026-01-13 21:45:49 +0900

    DXGIを書き終えた

---
+++ Game_Pro/Game_Pro/DXGI.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <d3d12.h>
+#include <dxgi1_4.h>
+#include <wrl/client.h>
+
+class DXGI final
+{
+public:
+
+	DXGI() = default;
+
+	~DXGI() = default;
+
+	[[nodiscard]] bool setDisplayAdapter() noexcept;
+
+	[[nodiscard]] IDXGIFactory4* factory() const noexcept;
+
+	[[nodiscard]] IDXGIAdapter1* displayAdapter() const noexcept;
+
+private:
+	Microsoft::WRL::ComPtr<IDXGIFactory4> dxgiFactory_{};
+	Microsoft::WRL::ComPtr<IDXGIAdapter1> dxgiAdapter_{};
+};
+
