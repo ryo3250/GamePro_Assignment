3377527dac4a0752c11200755d03b781a6c42cec
ryo3250 <ryoman1002@icloud.com>
2026-01-19 21:21:10 +0900

    シェーダー作成

---
+++ Game_Pro/Game_Pro/shader.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "device.h"
+
+class shader final {
+public:
+    shader() = default;
+
+    ~shader() = default;
+
+    [[nodiscard]] bool create() noexcept;
+
+    [[nodiscard]] ID3DBlob* vertexShader() const noexcept;
+
+    [[nodiscard]] ID3DBlob* pixelShader() const noexcept;
+
+private:
+    Microsoft::WRL::ComPtr<ID3DBlob> vertexShader_{};
+    Microsoft::WRL::ComPtr<ID3DBlob> pixelShader_{};
+};
