da6bd09e02db217c834d4b32d8c6689b3ec6fc7d
ryo3250 <ryoman1002@icloud.com>
2026-01-19 21:22:08 +0900

    保存し忘れ

---
+++ Game_Pro/Game_Pro/shader.cpp
@@ -0,0 +1,11 @@
+#include "shader.h"
+#include <cassert>
+#include <string>
+
+#include<d3dcompiler.h>
+#pragma comment(lib,"d3dcompiler.lib")
+
+[[nodiscard]] bool shader::create() noexcept 
+{
+	const std::string filePath = 
+}
