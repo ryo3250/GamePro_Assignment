1e3df4105c30f608f134a98794b3496a338c8b94
ryo3250 <ryoman1002@icloud.com>
2026-01-20 20:50:47 +0900

    シェーダ完成

--- Game_Pro/Game_Pro/shader.cpp
+++ Game_Pro/Game_Pro/shader.cpp
@@ -7,5 +7,43 @@
 
 [[nodiscard]] bool shader::create() noexcept 
 {
-	const std::string filePath = 
+    const std::string filePath = "asset/shader.hlsl";
+    const std::wstring temp     = std::wstring(filePath.begin(), filePath.end());
+
+    ID3DBlob* error{};
+
+    auto res = D3DCompileFromFile(temp.data(), nullptr,nullptr, "vs", "vs_5_0", D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, 0, &vertexShader_, &error);
+    if (FAILED(res)) {
+        char* p = static_cast<char*>(error->GetBufferPointer());
+        assert(false && "頂点シェーダのコンパイルに失敗しました");
+    }
+    res = D3DCompileFromFile(temp.data(), nullptr, nullptr, "ps", "ps_5_0", D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, 0, &pixelShader_, &error);
+    if (FAILED(res)) {
+        char* p = static_cast<char*>(error->GetBufferPointer());
+        assert(false && "ピクセルシェーダのコンパイルに失敗しました");
+    }
+
+    if (error) {
+        error->Release();
+    }
+
+    return true;
+}
+
+[[nodiscard]] ID3DBlob* shader::vertexShader() const noexcept 
+{
+    if (!vertexShader_) {
+        assert(false && "頂点シェーダが未作成です");
+    }
+
+    return vertexShader_.Get();
+}
+
+[[nodiscard]] ID3DBlob* shader::pixelShader() const noexcept 
+{
+    if (!pixelShader) {
+        assert(false && "ピクセルシェーダが未作成です");
+    }
+
+    return pixelShader_.Get();
 }
